4c8495b9f809a481493cf3f468c19795240c24ab
Juanma-v-b <juanma.testing.projects@gmail.com>
2025-05-03 15:54:31 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 //(producción)
 export const environment = {
   production: true,
-  apiUrl: 'https://web-production-3b90.up.railway.app/' // backend en producción
+  apiUrl: 'https://web-production-3b90.up.railway.app' // backend en producción
 };
